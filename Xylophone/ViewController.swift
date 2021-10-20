f2b4bd0d83ac23503cfed18969e3b6fe5f9b3be9
Andrey <nefedovandrey1997@gmail.com>
2021-10-20 14:53:35 +0300

    add delay

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+            
         playSound(soundName: sender.currentTitle ?? "")
     }
     
@@ -25,6 +31,5 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
     }
 }
